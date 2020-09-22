8bcef81525e797d89ac3bad632e6c6db435437ff
Sebastian Agudelo Mejia - Ceiba Software <sebastian.mejia@ceiba.local>
2020-09-22 15:07:27 -0500

    Inicializa

---
+++ TallerElectrodomestico-servidor/src/main/java/com/ceiba/electrodomestico/TallerElectrodomesticoApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.electrodomestico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerElectrodomesticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerElectrodomesticoApplication.class, args);
+	}
+
+}
---
+++ TallerElectrodomestico-servidor/src/test/java/com/ceiba/electrodomestico/TallerElectrodomesticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.electrodomestico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerElectrodomesticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
