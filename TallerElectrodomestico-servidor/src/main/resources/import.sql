1d065727677be04fd20d3f68d25eea635a6e541c
Sebastian Agudelo Mejia - Ceiba Software <sebastian.mejia@ceiba.local>
2020-09-29 12:47:00 -0500

    Se crea el home y la seccion de crear registros al igual que el menu y el footer

--- TallerElectrodomestico-servidor/src/main/resources/import.sql
+++ TallerElectrodomestico-servidor/src/main/resources/import.sql
@@ -8,14 +8,3 @@ INSERT INTO tipo_electrodomestico(id, marca, descripcion) VALUES ( 4, 'fff', 'Li
 
 INSERT INTO tipo_electrodomestico(id, marca, descripcion) VALUES ( 5, 'fff', 'sanduchera');
 
-INSERT INTO registro(id, cedula_cliente, estado_registro, fecha_ingreso, fecha_salida, nombre_cliente, telefono, valor_pagar,entidad_tipo_electrodomestico_id) VALUES(1, '1036960221', false, '2020-05-27', null, 'sebastian', '5321133', 0, 1)
-
-INSERT INTO registro(id, cedula_cliente, estado_registro, fecha_ingreso, fecha_salida, nombre_cliente, telefono, valor_pagar,entidad_tipo_electrodomestico_id) VALUES(2, '1036960221', false, '2020-05-27', null, 'sebastian', '5321133', 0, 2)
-
-INSERT INTO registro(id, cedula_cliente, estado_registro, fecha_ingreso, fecha_salida, nombre_cliente, telefono, valor_pagar,entidad_tipo_electrodomestico_id) VALUES(3, '1036960221', false, '2020-05-27', null, 'sebastian', '5321133', 0, 3)
-
-INSERT INTO registro(id, cedula_cliente, estado_registro, fecha_ingreso, fecha_salida, nombre_cliente, telefono, valor_pagar,entidad_tipo_electrodomestico_id) VALUES(4, '1036960221', true, '2020-05-27', '2020-05-30', 'sebastian', '5321133', 1000, 1)
-
-INSERT INTO registro(id, cedula_cliente, estado_registro, fecha_ingreso, fecha_salida, nombre_cliente, telefono, valor_pagar,entidad_tipo_electrodomestico_id) VALUES(5, '1036960221', true, '2020-05-27', '2020-05-30', 'sebastian', '5321133', 2000, 2)
-
-INSERT INTO registro(id, cedula_cliente, estado_registro, fecha_ingreso, fecha_salida, nombre_cliente, telefono, valor_pagar,entidad_tipo_electrodomestico_id) VALUES(6, '1036960221', true, '2020-05-27', '2020-05-30', 'sebastian', '5321133', 3000, 3)
